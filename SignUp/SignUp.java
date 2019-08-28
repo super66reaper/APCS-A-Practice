76df4d3d77982a7b8a11df966e4766acf7e797cc
Matthew Johnson <super66reaper@gmail.com>
2019-08-28 17:23:14 -0600

    finished signup practice assigned from mr p

--- SignUp/SignUp.java
+++ SignUp/SignUp.java
@@ -5,13 +5,18 @@ public class SignUp {
         Scanner scan = new Scanner(System.in);
         String username;
         String password;
+        Boolean success;
         
         System.out.println("Enter Username:");
         username = scan.nextLine();
+
         if (username.equals("admin person") || username.equals("skater_gurl17")) {
             System.out.println("Username Taken");
             System.exit(0);
         }
+
+        System.out.println("Username accepted");
+
         System.out.println("Enter Password:");
         System.out.println("Rules:");
         System.out.println("Must start with vowel");
@@ -19,14 +24,47 @@ public class SignUp {
         System.out.println("Must not have username in password");
         System.out.println("Numbers are allowed, but only as the last two places");
         password = scan.nextLine();
+        
+        password = password.trim();
+
         String firstLet;
         firstLet = password.substring(0,1);
         if (firstLet.equals("a") || firstLet.equals("e") || firstLet.equals("i") || firstLet.equals("o") || firstLet.equals("u")) {
-            
+            if (password.contains("#") || password.contains("!") || password.contains("$") || password.contains("&") || password.contains("*")) {
+                if (!password.contains(username)) {
+                    String passNoLast;
+                    int passLength = password.length();
+                    passNoLast = password.substring(0, passLength - 2);
+
+                    if (!passNoLast.matches(".*\\d.*")) {
+                        System.out.println("Password accepted");
+                        success = true;
+                    }
+                    else {
+                        System.out.println("Password doesn't Fit Rules");
+                        System.exit(0);
+                    }
+                    
+                }
+                else {
+                    System.out.println("Password doesn't Fit Rules");
+                    System.exit(0);
+                }
+            }
+            else {
+                System.out.println("Password doesn't Fit Rules");
+                System.exit(0);
+            }
         }
         else {
-            System.out.println("Doesn't Fit Rules");
+            System.out.println("Password doesn't Fit Rules");
             System.exit(0);
+        }
+
+        if (success = true) {
+            System.out.println("Account successfully created");
+            System.out.println("Username: " + username);
+            System.out.println("Password: " + password);
         }
     }
 }
