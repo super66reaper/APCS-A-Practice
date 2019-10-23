067d699ea7b4da02c820cff08b21971ec6260ce0
Matthew Johnson <super66reaper@gmail.com>
2019-10-23 12:13:09 -0600

    finish car problem

--- CarFuel/Car.java
+++ CarFuel/Car.java
@@ -4,12 +4,18 @@ public class Car {
     private String type;
     private int tankSize;
     private double time;
+    private double mpg;
+
+    final private double sedanStandard = 50;
+    final private double suvStandard = 40;
+    final private double truckStandard = 30;
 
     public Car(String make, String type, int tankSize, double time) {
         setMake(make);
         setType(type);
         setTankSize(tankSize);
         setTime(time);
+        setMpg();
     }
 
     //G&S
@@ -29,6 +35,10 @@ public class Car {
         return this.time;
     }
 
+    public double getMpg() {
+        return this.mpg;
+    }
+
     public void setMake(String make) {
         this.make = make;
     }
@@ -45,7 +55,34 @@ public class Car {
         this.time = time;
     }
 
-    public double carMPG() {
-        
+    public void setMpg() {
+        this.mpg = calculateMpg();
+    }
+
+    //Functions
+    public double calculateMpg() {
+        return (time * 60.0) / tankSize * 4.544;
+    }
+
+    public String reportStatus() {
+        if(type.equals("Sedan")) {
+            if(mpg >= sedanStandard) {
+                return "does";
+            }else {
+                return "does not";
+            }
+        }else if(type.equals("SUV")) {
+            if(mpg >= suvStandard) {
+                return "does";
+            }else {
+                return "does not";
+            }
+        }else {
+            if(mpg >= truckStandard) {
+                return "does";
+            }else {
+                return "does not";
+            }
+        }
     }
 }
--- CarFuel/CarFuel.java
+++ CarFuel/CarFuel.java
@@ -1,8 +1,17 @@
 public class CarFuel {
-    Car car1 = new Car("Takk", "Sedan", 50, 8);
-    Car car2 = new Car("Beklager", "Sedan", 40, 7.5);
-    Car car3 = new Car("Vakker", "SUV", 60, 5);
-    Car car4 = new Car("Stygg", "SUV", 55, 6);
-    Car car5 = new Car("Vanskellig", "Truck", 65, 4.75);
-    Car car6 = new Car("Lastebil", "Truck", 70, 5.5);
+    public static void main(String[] args) {
+        Car car1 = new Car("Takk", "Sedan", 50, 8);
+        Car car2 = new Car("Beklager", "Sedan", 40, 7.5);
+        Car car3 = new Car("Vakker", "SUV", 60, 5);
+        Car car4 = new Car("Stygg", "SUV", 55, 6);
+        Car car5 = new Car("Vanskellig", "Truck", 65, 4.75);
+        Car car6 = new Car("Lastebil", "Truck", 70, 5.5);
+    
+        System.out.println("The " + car1.getMake() + " " + car1.getType() + " with " + car1.getMpg() + " " + car1.reportStatus() + " fit the government standards.");
+        System.out.println("The " + car2.getMake() + " " + car2.getType() + " with " + car2.getMpg() + " " + car2.reportStatus() + " fit the government standards.");
+        System.out.println("The " + car3.getMake() + " " + car3.getType() + " with " + car3.getMpg() + " " + car3.reportStatus() + " fit the government standards.");
+        System.out.println("The " + car4.getMake() + " " + car4.getType() + " with " + car4.getMpg() + " " + car4.reportStatus() + " fit the government standards.");
+        System.out.println("The " + car5.getMake() + " " + car5.getType() + " with " + car5.getMpg() + " " + car5.reportStatus() + " fit the government standards.");
+        System.out.println("The " + car6.getMake() + " " + car6.getType() + " with " + car6.getMpg() + " " + car6.reportStatus() + " fit the government standards.");
+    }
 }
